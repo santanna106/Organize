25f28d3edb079b20581762b515c8ec1a8a5e36af
Gabriel Andrade <gabriel.santana@gmail.com>
2020-11-11 04:27:45 -0300

    Primeiro Commit

---
+++ app/src/main/java/com/example/organize/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.organize;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
+
+public class MainActivity extends IntroActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setButtonNextVisible(false);
+        setButtonBackVisible(false);
+
+        addSlide(new FragmentSlide.Builder()
+                .background(R.color.white)
+                .fragment(R.layout.intro_um)
+                .build());
+
+
+        addSlide(new FragmentSlide.Builder()
+                .background(R.color.white)
+                .fragment(R.layout.intro_dois)
+                .build());
+
+    }
+}
